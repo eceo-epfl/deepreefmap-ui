9fa9570f9df2129abccc59f6cd0d7b268490dd32
Evan Thomas <evan.thomas@epfl.ch>
2023-10-19 12:16:20 +0200

    Add Binntal sampling site

--- src/Map.tsx
+++ src/Map.tsx
@@ -3,19 +3,25 @@ import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
 export const LocationField = ({ source }) => {
     const record = useRecordContext(); // use the RecordContext created by <Show>
+    console.log(record)
+    const sensors = record["sensors"];
 
+    console.log(sensors);
     if (!record) return null;
     return (
 
         <MapContainer
             style={{ width: '100%', height: '700px' }}
             center={record[source]}
-            zoom={13}
+            zoom={17}
             scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            {sensors.map((sensor, index) => (
+                <Marker key={index} position={sensor["location"]} />
+            ))}
             <Marker
                 position={record[source]} />
         </MapContainer>
