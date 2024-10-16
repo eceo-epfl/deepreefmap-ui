b3c360130fbabedefbf54898a840edb4dd3d0e14
Evan Thomas <evan.thomas@epfl.ch>
2024-10-16 16:16:46 +0200

    Constrain fields in transects

--- src/transects/TransectCreate.tsx
+++ src/transects/TransectCreate.tsx
@@ -1,47 +1,67 @@
-/* eslint react/jsx-key: off */
 import {
     Create,
     SimpleForm,
-    useAuthProvider,
-    useCreate,
-    useDataProvider,
-    Toolbar,
-    SaveButton,
-    useRedirect,
-    Button,
-    TextField,
     TextInput,
-    ReferenceInput,
-    SelectInput,
     required,
-    ArrayInput,
-    SimpleFormIterator,
+    minValue,
+    maxValue,
     NumberInput,
 } from 'react-admin';
-import 'react-dropzone-uploader/dist/styles.css'
-import { FilePond, registerPlugin } from 'react-filepond';
+import { Grid, Typography } from '@mui/material';
+import 'react-dropzone-uploader/dist/styles.css';
 import 'filepond/dist/filepond.min.css';
-import Dropzone from 'react-dropzone-uploader'
-import { useState, useEffect, useRef } from 'react';
-
 
 const TransectCreate = () => {
-
     return (
         <Create redirect="show">
-            <SimpleForm >
-                <TextField source="id" />
-                <TextInput source="name" validate={[required()]} />
-                <TextInput source="description" />
-                <NumberInput source="latitude_start" />
-                <NumberInput source="longitude_start" />
-                <NumberInput source="latitude_end" />
-                <NumberInput source="longitude_end" />
-                <NumberInput source="length" label="length (m)" />
-                <NumberInput source="depth" label="depth (m)" />
+            <SimpleForm>
+                {/* Basic Information */}
+                <Typography variant="h6" gutterBottom>
+                    Basic Information
+                </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <TextInput source="name" validate={[required()]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextInput source="description" multiline fullWidth />
+                    </Grid>
+                </Grid>
+
+                {/* Location */}
+                <Typography variant="h6" gutterBottom style={{ marginTop: '16px' }}>
+                    Location
+                </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="latitude_start" validate={[required(), minValue(-90), maxValue(90)]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="longitude_start" validate={[required(), minValue(-180), maxValue(180)]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="latitude_end" validate={[required(), minValue(-90), maxValue(90)]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="longitude_end" validate={[required(), minValue(-180), maxValue(180)]} fullWidth />
+                    </Grid>
+                </Grid>
+
+                {/* Measurements */}
+                <Typography variant="h6" gutterBottom style={{ marginTop: '16px' }}>
+                    Measurements
+                </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="length" label="length (m)" validate={[minValue(0)]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="depth" label="depth (m)" validate={[minValue(0)]} fullWidth />
+                    </Grid>
+                </Grid>
             </SimpleForm>
-        </Create >
-    )
+        </Create>
+    );
 };
 
 export default TransectCreate;
--- src/transects/TransectEdit.tsx
+++ src/transects/TransectEdit.tsx
@@ -3,29 +3,66 @@ import {
     SimpleForm,
     TextInput,
     NumberInput,
-    ArrayInput,
-    SimpleFormIterator,
     minValue,
+    maxValue,
     required,
-    SelectInput,
 } from 'react-admin';
+import { Grid, Typography } from '@mui/material';
 
 const TransectEdit = () => {
     return (
         <Edit redirect="show">
             <SimpleForm>
-                <TextInput source="id" disabled />
-                <TextInput source="name" validate={[required()]} />
-                <TextInput source="description" />
-                <NumberInput source="length" label="length (m)" />
-                <NumberInput source="depth" label="depth (m)" />
-                <NumberInput source="latitude_start" validate={[required()]} />
-                <NumberInput source="longitude_start" validate={[required()]} />
-                <NumberInput source="latitude_end" validate={[required()]} />
-                <NumberInput source="longitude_end" validate={[required()]} />
+                {/* Basic Information */}
+                <Typography variant="h6" gutterBottom>
+                    Basic Information
+                </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <TextInput source="id" disabled fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextInput source="name" validate={[required()]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextInput source="description" multiline />
+                    </Grid>
+                </Grid>
+
+                {/* Location */}
+                <Typography variant="h6" gutterBottom style={{ marginTop: '16px' }}>
+                    Location
+                </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="latitude_start" validate={[required(), minValue(-90), maxValue(90)]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="longitude_start" validate={[required(), minValue(-180), maxValue(180)]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="latitude_end" validate={[required(), minValue(-90), maxValue(90)]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="longitude_end" validate={[required(), minValue(-180), maxValue(180)]} fullWidth />
+                    </Grid>
+                </Grid>
+
+                {/* Measurements */}
+                <Typography variant="h6" gutterBottom style={{ marginTop: '16px' }}>
+                    Measurements
+                </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="length" label="length (m)" validate={[minValue(0)]} fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <NumberInput source="depth" label="depth (m)" validate={[minValue(0)]} fullWidth />
+                    </Grid>
+                </Grid>
             </SimpleForm>
         </Edit>
-    )
+    );
 };
 
 export default TransectEdit;
