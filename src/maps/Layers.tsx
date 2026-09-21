354ebaa2d3e6733e6dc70e01ab096204ba4dec72
Evan Thomas <evan.thomas@epfl.ch>
2026-09-21 08:55:36 +0200

    Set OSM as default

--- src/maps/Layers.tsx
+++ src/maps/Layers.tsx
@@ -1,13 +1,11 @@
 import { LayersControl, TileLayer } from 'react-leaflet';
-import { useTheme } from 'react-admin';
 
 export const BaseLayers = () => {
     const { BaseLayer } = LayersControl;
-    const [theme] = useTheme();
 
     return (
         <LayersControl>
-            <BaseLayer name="CARTO Dark" checked={theme === 'dark'}>
+            <BaseLayer name="CARTO Dark">
                 <TileLayer
                     url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
@@ -16,7 +14,7 @@ export const BaseLayers = () => {
                     zIndex={0}
                 />
             </BaseLayer>
-            <BaseLayer name="OpenStreetMap" checked={theme !== 'dark'}>
+            <BaseLayer name="OpenStreetMap" checked>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
